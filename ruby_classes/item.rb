4ceab2c0106ac7832ea62cfe6bc01e45e8c6e2ee
Somdotta07 <jhilick.s@gmail.com>
2022-02-07 13:57:04 +0100

    added can_be_archived method to item class

--- ruby_classes/item.rb
+++ ruby_classes/item.rb
@@ -1,32 +1,37 @@
 class Item
-  attr_reader :genre, :autor, :source, :label
-  attr_accessor :id, :publish_date, :archived
+  attr_reader :genre, :author, :source, :label
+  attr_accessor :publish_date
 
   def intialize(id:, publish_date:, archived: false)
     @id = id || Random.rand(1..1000)
-    @publish_date: publish_date
-    @archived: archived
+    @publish_date = publish_date
+    @archived = archived
   end
 
-  def genre(value)
-    @genre = value
+  def genre=(genre)
+    @genre = genre
     @genre.items.push(self) unless @genre.items.include?(self)
   end
 
-  def autor(value)
-    @autor = value
+  def author=(author)
+    @autor = author
     @autor.items.push(self) unless @autor.items.include?(self)
   end
 
-  def source(value)
-    @source = value
+  def source=(source)
+    @source = source
     @source.items.push(self) unless @source.items.include?(self)
   end
 
-  def label(value)
-    @label = value
-    @label.items.push(self) unless @source.items.include?(self)
+  def label=(label)
+    @label = label
+    @label.items.push(self) unless @label.items.include?(self)
   end
 
-  
+  private
+
+  def can_be_archived?
+    current_year = Time.new.year
+    current_year - @publish_date[:year] > 10
+  end
 end
