a4f1952ae38d2c56e950d088bb382677e4b9712a
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-09 05:56:48 +0800

    Create add_new_author method

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -3,4 +3,16 @@ module AuthorModule
     print "#{request_text}: "
     gets.chomp
   end
+
+  def add_new_author
+    puts "\n-------------------------------"
+    puts "\nINPUT AUTHOR INFO.\n\n"
+
+    first_name = capture_input('First Name:')
+    last_name = capture_input(' Last Name:')
+
+    @app.add_author([first_name, last_name])
+    puts "\nNew Author Added!"
+    puts "\n-------------------------------"
+  end
 end
