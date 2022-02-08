b297cf094cc8fd7f6f11c93179f9fa52172f6565
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-09 05:50:16 +0800

    Create method add_new_game

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -4,4 +4,16 @@ module GameModule
     gets.chomp
   end
   
+  def add_new_game
+    puts "\n-------------------------------"
+    puts "\nENTER GAME DETAILS\n\n"
+
+    last_played_at = get_input('Input the last date you played the game')
+    multiplayer = get_input('Is the game Multiplayer? (enter Y for "Yes" anything except Y for "No"')
+
+    multiplayer = multiplayer == 'Y' || true
+    @app.add_game([multiplayer, last_played_at])
+    puts "\nNew Game Added!"
+    puts "\n-------------------------------"
+  end
 end
